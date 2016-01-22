5b6ef494e85c7f5c0f5db89d454f0a7d4206ce47
Brenda Wallace <brenda@rabidtech.co.nz>
2016-01-22 15:37:04 +1300

    move more into config

--- lib/mysensors/gateway/openhab.py
+++ lib/mysensors/gateway/openhab.py
@@ -1,22 +1,23 @@
-class configuration:
-    url = None
+class Config:
+    URL = None
+    URL_PATTERN = 'http://{url}/rest/items/Arduino_{node_id}{sub_type}/state'
+    HTTP_HEADERS = {'Content-Type': 'text/plain'}
 
 
 def configure_for_openhab(url=''):
     configuration.url = url
 
 
-def send_status_to_openhab(key, value, station):
-    url = 'http://{host}:{port}/rest/items/Arduino_{station}{key}/state'.format(
-        host=openhab_host,
-        port=openhab_port,
-        station=station,
-        key=key)
+def send_status_to_openhab(node_id, sub_type, payload):
+    url = Config.URL_PATTERN.format(url=Config.URL,
+                                    node_id=node_id,
+                                    sub_type=sub_type)
 
     print url
-    print value
-    response = requests.put(url, headers={'Content-Type': 'text/plain'},
-                            data=str(value))
+    print payload
+    response = requests.put(url,
+                            headers=Config.HTTP_HEADERS,
+                            data=str(payload))
     print response.text
 
     if response.status_code != requests.codes.ok:
---
+++ seriallistener.py
@@ -0,0 +1,5 @@
+from mysensors.gateway.openhab import configure_for_openhab, send_status_to_openhab
+from mysensors import listen_for_data
+
+configure_for_openhab(url="http://127.0.0.1:8080")
+listen_for_data(port='/dev/ttyUSB0', handler=send_status_to_openhab)
