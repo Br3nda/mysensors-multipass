0f8ddcd00c0b5f3d597d501e353456cb9e6085ec
Brenda Wallace <brenda@rabidtech.co.nz>
2016-01-22 15:37:32 +1300

    example listener

--- seriallistener.py
+++ seriallistener.py
@@ -1,5 +1,5 @@
 from mysensors.gateway.openhab import configure_for_openhab, send_status_to_openhab
-from mysensors import listen_for_data
+from mysensors.listener import listen_for_data
 
-configure_for_openhab(url="http://127.0.0.1:8080")
-listen_for_data(port='/dev/ttyUSB0', handler=send_status_to_openhab)
+# configure_for_openhab(url="http://127.0.0.1:8080")
+listen_for_data(port='/dev/ttyUSB0') #, handler=send_status_to_openhab)
