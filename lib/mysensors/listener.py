99602635f0fcab36816ab36da54545ed7f0b6559
Brenda Wallace <brenda@wallace.net.nz>
2016-01-27 22:45:30 +1300

    use the new Message class

--- lib/mysensors/listener.py
+++ lib/mysensors/listener.py
@@ -1,33 +1,22 @@
 import serial
 import requests
-
+from mysensors.message import Message
 # The pattern is:
 # node-id;child-sensor-id;message-type;ack;sub-type;payload\n
 
 
-def default_handler(node_id, sub_type, payload):
-    print "Key = '{node_id} {sub_type}', payload='{payload}'".format(
-        node_id=node_id,
-        sub_type=sub_type,
-        payload=payload)
+def default_handler(message):
+    print message
 
 
 def listen_for_data(port, speed=115200, handler=default_handler):
     print "listening"
     ser = serial.Serial(port, speed)
     while True:
-        try:
-            line = ser.readline().strip()
-            print line
-            # node-id;child-sensor-id;message-type;ack;sub-type;payload\n
-            (node_id,
-                child_sensor_id,
-                messsage_type,
-                ack,
-                sub_type,
-                payload) = line.split(';')
-
-            if messsage_type == M_SET:
-                handler(node_id=node_id, sub_type=sub_type, payload=payload)
-        except Exception, e:
-            print e
+        # try:
+        line = ser.readline()
+        print line
+        message = Message(line)
+        handler(message)
+        # except Exception, e:
+            # print e
