6ef7cf7b4f66ff4ea9021b129e3203592ebdad60
Brenda Wallace <brenda@wallace.net.nz>
2016-01-27 21:20:20 +1300

    Message class structure

--- lib/mysensors/message.py
+++ lib/mysensors/message.py
@@ -1,2 +1,15 @@
 class Message:
+  ##
+  # node-id
+  # ;child-sensor-id
+  # ;message-type
+  # ;ack
+  # ;sub-type
+  # ;payload
+  # \n
+  @classmethod
+  def decode(cls, sentence):
+    message = Message(node_id, child_sensor_id, message_type, ack, sub_type, payload)
+
+  def __init__(self, node_id, child_sensor_id, message_type, ack, sub_type, payload):
     pass
