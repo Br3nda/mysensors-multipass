4bc5f1dc901218315ac0e2fe35a1aa0317fcd162
Brenda Wallace <brenda@wallace.net.nz>
2016-01-27 22:59:19 +1300

    cleaned up message type decoding

--- lib/mysensors/__init__.py
+++ lib/mysensors/__init__.py
@@ -19,33 +19,39 @@ MESSAGE_TYPES = {
   'M_STREAM': 4,  # Used for OTA firmware updates
 
 ### PRESENTATION
+}
 
-  'S_DOOR': '0',  # Door and window sensors
-  'S_MOTION': '1',  # Motion sensors
-  'S_SMOKE': '2',  # Smoke sensor
-  'S_LIGHT': '3',  # Light Actuator (on/off)
-  'S_DIMMER': '4',  # Dimmable device of some kind
-  'S_COVER': '5',  # Window covers or shades
-  'S_TEMP': '6',  # Temperature sensor
-  'S_HUM': '7',  # Humidity sensor
-  'S_BARO': '8',  # Barometer sensor (Pressure)
-  'S_WIND': '9',  # Wind sensor
-  'S_RAIN': '10',  # Rain sensor
-  'S_UV': '11',  # UV sensor
-  'S_WEIGHT': '12',  # Weight sensor for scales etc.
-  'S_POWER': '13',  # Power measuring device ',like power meters
-  'S_HEATER': '14',  # Heater device
-  'S_DISTANCE': '15',  # Distance sensor
-  'S_LIGHT_LEVEL': '16',  # Light sensor
-  'S_ARDUINO_NODE': '17',  # Arduino node device
-  'S_ARDUINO_RELAY': '18',  # Arduino repeating node device
-  'S_LOCK': '19',  # Lock device
-  'S_IR': '20',  # Ir sender/receiver device
-  'S_WATER': '21',  # Water meter
-  'S_AIR_QUALITY': '22',  # Air quality sensor e.g. MQ-2
-  'S_CUSTOM': '23',  # Use this for custom sensors where no other fits.
-  'S_DUST': '24',  # Dust level sensor
-  'S_SCENE_CONTROLLER': '25',  # Scene controller device':
+PRESENTATION  = {
+  'S_DOOR': 0,  # Door and window sensors
+  'S_MOTION': 1,  # Motion sensors
+  'S_SMOKE': 2,  # Smoke sensor
+  'S_LIGHT': 3,  # Light Actuator (on/off)
+  'S_DIMMER': 4,  # Dimmable device of some kind
+  'S_COVER': 5,  # Window covers or shades
+  'S_TEMP': 6,  # Temperature sensor
+  'S_HUM': 7,  # Humidity sensor
+  'S_BARO': 8,  # Barometer sensor (Pressure)
+  'S_WIND': 9,  # Wind sensor
+  'S_RAIN': 10,  # Rain sensor
+  'S_UV': 11,  # UV sensor
+  'S_WEIGHT': 12,  # Weight sensor for scales etc.
+  'S_POWER': 13,  # Power measuring device ,like power meters
+  'S_HEATER': 14,  # Heater device
+  'S_DISTANCE': 15,  # Distance sensor
+  'S_LIGHT_LEVEL': 16,  # Light sensor
+  'S_ARDUINO_NODE': 17,  # Arduino node device
+  'S_ARDUINO_RELAY': 18,  # Arduino repeating node device
+  'S_LOCK': 19,  # Lock device
+  'S_IR': 20,  # Ir sender/receiver device
+  'S_WATER': 21,  # Water meter
+  'S_AIR_QUALITY': 22,  # Air quality sensor e.g. MQ-2
+  'S_CUSTOM': 23,  # Use this for custom sensors where no other fits.
+  'S_DUST': 24,  # Dust level sensor
+  'S_SCENE_CONTROLLER': 25,  # Scene controller device':
+
+}
+
+SET_REQ = {
   #,## set, req 
   'V_TEMP': 0,  # Temperature
   'V_HUM': 1,  # Humidity
@@ -95,7 +101,17 @@ MESSAGE_TYPES = {
 def decode_message_type(message_type):
   return [k for (k, v) in MESSAGE_TYPES.iteritems() if v == message_type][0]
   
+def decode_sub_type(message_type, sub_type):
+  if message_type == 'M_PRESENTATION':
+    lookup_dict = PRESENTATION
+  elif message_type in ['M_SET', 'M_REQ'] :
+    lookup_dict = SET_REQ
+  return [k for (k, v) in lookup_dict.iteritems() if v == sub_type][0]
 
 def test_decode_message_type():
-  assert decode_message_type(38) == 'V_VOLTAGE'
-  assert decode_message_type(24) == "V_VAR1"
+  assert decode_message_type(0) == 'M_PRESENTATION'
+  assert decode_message_type(2) == "M_REQ"
+
+def test_decode_sub_type():
+  assert decode_sub_type('M_SET', 38) == 'V_VOLTAGE'
+  assert decode_sub_type('M_PRESENTATION', 19) == "S_LOCK"
