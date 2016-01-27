158608990f81684bd77ebd358b6fac29928519b0
Brenda Wallace <brenda@wallace.net.nz>
2016-01-27 22:43:49 +1300

    seriallistener is now example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from mysensors.gateway.openhab import configure_for_openhab, send_status_to_openhab
+from mysensors.gateways.openhab import configure_for_openhab, send_status_to_openhab
 from mysensors.listener import listen_for_data
 
 # configure_for_openhab(url="http://127.0.0.1:8080")
